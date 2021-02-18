bdcd9cd789125e88bab18cc48442f57291c17450
Dimanopoulos Michalis <E81545@NBG.GR>
2021-02-18 11:05:09 +0200

    Add project structure and healthCheck action

---
+++ src/TinyBank.Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TinyBank.Types.Requests;
+
+namespace TinyBank.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        public HealthCheckController() { }
+
+        [HttpGet]
+        public IActionResult HealthCheck(GenericRequest request)
+        {
+            return Content("The ASP.NET Core Api is running!");
+        }
+    }
+}
---
+++ src/TinyBank.Types/Requests/GenericRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace TinyBank.Types.Requests
+{
+    [DataContract]
+    public class GenericRequest
+    {
+        [DataMember(Name = "userName")]
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+    }
+}
