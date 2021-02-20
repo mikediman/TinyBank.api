30aa34f90275087ba02200bc6b94c2e7266c451a
Dimanopoulos Michalis <E81545@NBG.GR>
2021-02-20 10:37:26 +0200

    Add Dependancy Injection

---
+++ src/TinyBank.Implementation/ServiceExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+using TinyBank.Implementation.Config;
+using TinyBank.Implementation.Config.Extensions;
+using TinyBank.Implementation.Database;
+using TinyBank.Interfaces;
+
+namespace TinyBank.Implementation.ServiceExtensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAppServices(this IServiceCollection @this, IConfiguration configuration)
+        {
+            @this.AddSingleton<AppConfig>(configuration.ReadAppConfiguration());
+
+            @this.AddDbContext<TinyBankDbContext>(
+                (serviceProvider, optionsBuilder)=> {
+                    var appConfig = serviceProvider.GetRequiredService<AppConfig>();
+                    optionsBuilder.UseSqlServer(appConfig.TinyBankDbConnectionString);
+                });
+
+            @this.AddScoped<IUserService, UserService>();
+        }
+    }
+}
